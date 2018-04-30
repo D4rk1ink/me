e688126b8c385cf80c218062570417a6c52ad774
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-05-01 04:03:02 +0700

    Update responsive and add cloud background

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -11,10 +11,14 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    flex: 1;
     width: 100%;
-    height: 100%;
+    height: 50vw;
+    @media only screen and (min-width: 45rem) {
+        height: 60vw;
+    }
     @media only screen and (min-width: 75rem) {
+        flex: 1;
+        height: 100%;
         flex: .7;
     }
 `
@@ -27,7 +31,7 @@ const ImagesWrapper = styled.div`
     width: 30%;
     height: 100%;
     @media only screen and (min-width: 45rem) {
-        height: 100%;
+        width: 30%;
     }
     @media only screen and (min-width: 75rem) {
         width: 50%;
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import color from 'src/constants/color'
+import Fade from 'react-reveal/Fade'
 import StoryWrapper from 'src/components/StoryWrapper'
 import Title from 'src/components/Title'
 import ProfileImage from 'src/components/ProfileImage'
 import ProfileDetail from 'src/components/ProfileDetail'
+import CloudTwoSVG from 'src/images/cloud-2.svg'
 
 const Wrapper = styled(StoryWrapper)`
-    height: 120vw;
     background-color: ${color.azure};
+    overflow: hidden;
 `
 
 const Content = styled.div`
@@ -24,10 +26,27 @@ const Content = styled.div`
     }
 `
 
+const CloudBackground = styled.div`
+    position: absolute;
+    width: 60vw;
+    bottom: -7vw;
+    right: -3vw;
+`
+
+const CloudImage = styled.img`
+    width: 100%;
+    filter: invert(0.22) sepia(1) saturate(1) hue-rotate(140deg);
+`
+
 class Profile extends Component {
     render () {
         return (
             <Wrapper>
+                <CloudBackground>
+                    <Fade bottom>
+                        <CloudImage src={CloudTwoSVG} />
+                    </Fade>
+                </CloudBackground>
                 <Title title={'Profile'} dark />
                 <Content>
                     <ProfileImage />
