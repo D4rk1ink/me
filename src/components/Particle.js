9de7a4c02eba028494dcd21bdc0fb40b299ebf9e
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-05-15 10:59:21 +0700

    Add background and moved particle

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -9,6 +9,21 @@ const Particle = styled.div`
     background-color: #FFFFFF;
     opacity: ${ ({ opacity }) => opacity };
     transition: opacity .5s;
+    animation: move-${ ({ move }) => move.key } 130s infinite;
+    @keyframes move-${ ({ move }) => move.key } {
+        0% {
+            transform: ${ ({ move }) => `translate(${move.a.x}vw, ${move.a.y}vh)`};
+        }
+        33% {
+            transform: ${ ({ move }) => `translate(${move.b.x}vw, ${move.b.y}vh)`};
+        }
+        66% {
+            transform: ${ ({ move }) => `translate(${move.c.x}vw, ${move.c.y}vh)`};
+        }
+        100% {
+            transform: ${ ({ move }) => `translate(${move.d.x}vw, ${move.d.y}vh)`};
+        }
+    }
 `
 
 const Ghost = styled.div`
@@ -20,18 +35,38 @@ const Ghost = styled.div`
     width: ${ ({ size }) => size }px;
     height: ${ ({ size }) => size }px;
     background-color: #00000000;
-    transform: ${ ({ translate }) => `translate3d(${translate.x}vw, ${translate.y}vh, ${translate.z}vw)`};
+    transform: ${ ({ location }) => `translateZ(${location.z}vw)`};
+    
 `
 
-export default ({ minSize = 2, maxSize = 10 }) => {
+export default ({ minSize = 1, maxSize = 4 }) => {
     const size = Math.random() * (maxSize - minSize) + minSize
-    const x = Math.random() * (140 - -40) + 0
-    const y = Math.random() * (140 - -40) + 0
+    const x = Math.random() * (140 - -40) + -40
+    const y = Math.random() * (140 - -40) + -40
     const z = Math.random() * (20 - 0) + 0
     const opacity = (Math.random() * (80 - 50) + 50) / 100
+    const move = {
+        key: Math.floor(Math.random() * 1e+10),
+        a: {
+            x: x,
+            y: y
+        },
+        b: {
+            x: x + Math.random() * (50 - -50) + -50,
+            y: y + Math.random() * (50 - -50) + -50
+        },
+        c: {
+            x: x + Math.random() * (50 - -50) + -50,
+            y: y + Math.random() * (50 - -50) + -50
+        },
+        d: {
+            x: x,
+            y: y
+        },
+    }
     return (
-        <Ghost size={size * 20} translate={{x,y,z}}>
-            <Particle size={size} opacity={opacity}/>
+        <Ghost size={size * 20} location={{x,y,z}}>
+            <Particle size={size} opacity={opacity} move={move}/>
         </Ghost>
     )
 }
--- src/containers/ParticlePanel.js
+++ src/containers/ParticlePanel.js
@@ -2,28 +2,49 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Menu from './Menu'
 import Particle from 'src/components/Particle'
+import BackgroundPNG from 'src/images/bg.png'
 
 const Wrapper = styled.div`
     position: fixed;
+    top: 0;
     width: 100vw;
     height: 100vh;
+`
+
+const BackgroundWrapper = styled.div`
+    position: absolute;
+    top: 0;
+`
+
+const Background = styled.img`
+    height: 100vh;
+`
+
+const ParticleWrapper = styled.div`
+    position: absolute;
+    top: 0;
 `
 
 class Header extends Component {
 
     constructor (props) {
         super(props)
-        this.size = Math.round(Math.random() * (150 - 100) + 100)
+        this.size = Math.round(Math.random() * (200 - 100) + 100)
     }
 
     render () {
         return (
-            <Wrapper id={'parallax'}>
-                {
-                    new Array(this.size).fill(null).map((item, i) => {
-                        return <Particle key={i} />
-                    })
-                }
+            <Wrapper>
+                <BackgroundWrapper>
+                    <Background src={BackgroundPNG}/>
+                </BackgroundWrapper>
+                <ParticleWrapper id={'parallax'}>
+                    {
+                        new Array(this.size).fill(null).map((item, i) => {
+                            return <Particle key={i} />
+                        })
+                    }
+                </ParticleWrapper>
             </Wrapper>
         )
     }
