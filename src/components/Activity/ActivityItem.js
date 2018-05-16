1c46ea5d159c1d1e371b3536f9b13ac1c55089f5
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-05-16 19:07:59 +0700

    Remove react-fontawesome

--- src/components/Activity/ActivityItem.js
+++ src/components/Activity/ActivityItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import FontAwesome from 'react-fontawesome'
 import color from 'src/constants/color'
 import Fade from 'react-reveal/Fade'
 
